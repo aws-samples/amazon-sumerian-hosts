04d50a7e7ec1f22fa09986c09de223b02cd2cc15
Kris Schultz <krisschultz@gmail.com>
2022-03-11 19:07:52 -0800

    Add support for local dev credentials file

--- packages/amazon-sumerian-hosts-babylon/examples/common/demo-credentials.template.js
+++ packages/amazon-sumerian-hosts-babylon/examples/common/demo-credentials.template.js
@@ -1,4 +1,5 @@
-// TODO: Update this value to match a Cognito Identity Pool created in your AWS
+// TODO: Make a copy of this file and name it "demo-credentials.js". Then,
+// update the value below to match a Cognito Identity Pool created in your AWS
 // account. The unauthenticated IAM role for the pool (usually ending in the
 // suffix "Unauth_Role") must have the following managed permissions policies
 // assigned to it:
