48bb9fcb7d68027197ceb84454185d87cb99bbf5
Kris Schultz <krisschultz@gmail.com>
2022-03-29 20:52:59 -0700

    Chatbot demo: Refactor ConversationController class to functions

--- packages/demos-babylon/src/chatbotDemo.js
+++ packages/demos-babylon/src/chatbotDemo.js
@@ -1,11 +1,9 @@
-import { HostObject, aws as awsFeatures } from '@amazon-sumerian-hosts/babylon';
+import { HostObject, aws as AwsFeatures } from '@amazon-sumerian-hosts/babylon';
 import DemoUtils from './common/demo-utils';
 import cognitoIdentityPoolId from '../../../demo-credentials';
 
 let host;
 let scene;
-let lex;
-let conversationController;
 
 async function createScene() {
   // Create an empty scene. IMPORTANT: Sumerian Hosts require use of the
@@ -40,23 +38,98 @@ async function createScene() {
     shadowGenerator.addShadowCaster(mesh);
   });
 
-  // ===== Initialize chatbot functionality =====
-
-  // Initialize chatbot access.
-  const { LexFeature } = awsFeatures;
+  // Initialize chatbot access. If you'd like to use this demo with a different chatbot, just change the
+  // botName and botAlias values below.
   const lexClient = new AWS.LexRuntime();
-  const botName = 'BookTrip';
-  const botAlias = 'Dev';
-  lex = new LexFeature(lexClient, { botName, botAlias });
-
-  conversationController = new ConversationController(host, lex);
+  const botConfig = {
+    botName: 'BookTrip',
+    botAlias: 'Dev'
+  }
+  lex = new AwsFeatures.LexFeature(lexClient, botConfig);
 
   initUi();
+  initConversationManagement();
   acquireMicrophoneAccess();
 
   return scene;
 }
 
+function initUi() {
+  // Set up interactions for UI buttons.
+  document.getElementById('startButton').onclick = () => startMainExperience();
+  document.getElementById('enableMicButton').onclick = () => acquireMicrophoneAccess();
+}
+
+/**
+ * Triggered when the user clicks the initial "start" button.
+ */
+function startMainExperience() {
+  showUiScreen('chatbotUiScreen');
+
+  // Speak a greeting to the user.
+  host.TextToSpeechFeature.play(
+    `Hello. How can I help?  You can say things like, "I'd like to rent a car," or, "Help me book a hotel".`
+  );
+}
+
+// ===== Chatbot functions =====
+
+let messageContainerEl;
+let transcriptTextEl;
+let lex;
+
+function initConversationManagement() {
+  // Use talk button events to start and stop recording.
+  const talkButton = document.getElementById('talkButton');
+  talkButton.onmousedown = () => lex.beginVoiceRecording();
+  talkButton.onmouseup = () => lex.endVoiceRecording();
+
+  // Use events dispatched by the LexFeature to present helpful user messages.
+  const { EVENTS } = AwsFeatures.LexFeature;
+  lex.listenTo(EVENTS.lexResponseReady, (response) => handleLexResponse(response));
+  lex.listenTo(EVENTS.recordBegin, () => hideUserMessages());
+  lex.listenTo(EVENTS.recordEnd, () => displayProcessingMessage());
+
+  // Create convenience references to DOM elements.
+  messageContainerEl = document.getElementById('userMessageContainer');
+  transcriptTextEl = document.getElementById('transcriptText');
+}
+
+/**
+ * Triggered whenever a response is received from the Lex chatbot.
+ * @param {object} response An object representing the Lex response. For a
+ * detailed description of this object's shape, see the documentation for the
+ * "data" callback argument described here:
+ * {@link https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/LexRuntime.html#postContent-property}
+ */
+function handleLexResponse(response) {
+  // Remove "processing" CSS class from message container.
+  messageContainerEl.classList.remove('processing');
+
+  // Display the user's speech input transcript.
+  displaySpeechInputTranscript(response.inputTranscript);
+
+  // Have the host speak the response from Lex.
+  host.TextToSpeechFeature.play(response.message);
+}
+
+function displaySpeechInputTranscript(text) {
+  transcriptTextEl.innerText = `“${text}”`;
+  messageContainerEl.classList.add('showingMessage');
+}
+
+function displayProcessingMessage() {
+  messageContainerEl.classList.add('processing');
+}
+
+function hideUserMessages() {
+  messageContainerEl.classList.remove('showingMessage');
+}
+
+/**
+ * Attempts to enable microphone access for Lex, triggering a browser permissions
+ * prompt if necessary.
+ */
 function acquireMicrophoneAccess() {
   showUiScreen('micInitScreen');
 
@@ -79,20 +152,7 @@ function acquireMicrophoneAccess() {
     });
 }
 
-function initUi() {
-  // Set up interactions for UI buttons.
-  document.getElementById('startButton').onclick = () => startMainExperience();
-  document.getElementById('enableMicButton').onclick = () => acquireMicrophoneAccess();
-}
-
-function startMainExperience() {
-  showUiScreen('chatbotUiScreen');
-
-  // Speak a greeting to the user.
-  host.TextToSpeechFeature.play(
-    `Hello. How can I help?  You can say things like, "I'd like to rent a car," or, "Help me book a hotel".`
-  );
-}
+// ===== Utility functions =====
 
 /**
  * Makes the specified UI screen visible and hides all other UI screens.
@@ -116,55 +176,6 @@ function setElementVisibility(id, visible) {
     element.classList.remove('hide');
   } else {
     element.classList.add('hide');
-  }
-}
-
-/**
- * The ConversationController manages user interactions with the chatbot.
- */
-class ConversationController {
-  constructor(host, lexFeature) {
-    this.host = host;
-    this.lex = lexFeature;
-
-    // Use talk button events to start and stop recording.
-    const talkButton = document.getElementById('talkButton');
-    talkButton.onmousedown = () => this.lex.beginVoiceRecording();
-    talkButton.onmouseup = () => this.lex.endVoiceRecording();
-
-    // Use events dispatched by the LexFeature to present helpful user messages.
-    const { EVENTS } = awsFeatures.LexFeature;
-    this.lex.listenTo(EVENTS.lexResponseReady, (response) => this._handleLexResponse(response));
-    this.lex.listenTo(EVENTS.recordBegin, () => this._hideUserMessages());
-    this.lex.listenTo(EVENTS.recordEnd, () => this._displayProcessingMessage());
-
-    // Create convenience references to DOM elements.
-    this.messageContainerEl = document.getElementById('userMessageContainer');
-    this.transcriptTextEl = document.getElementById('transcriptText');
-  }
-
-  _handleLexResponse(response) {
-    // Remove "processing" CSS class from message container.
-    this.messageContainerEl.classList.remove('processing');
-
-    // Display the user's speech input transcript.
-    this._displayTranscript(response.inputTranscript);
-
-    // Have the host speak the response from Lex.
-    this.host.TextToSpeechFeature.play(response.message);
-  }
-
-  _displayTranscript(text) {
-    document.getElementById('transcriptText').innerText = `“${text}”`;
-    this.messageContainerEl.classList.add('showingMessage');
-  }
-
-  _displayProcessingMessage() {
-    this.messageContainerEl.classList.add('processing');
-  }
-
-  _hideUserMessages() {
-    this.messageContainerEl.classList.remove('showingMessage');
   }
 }
 
