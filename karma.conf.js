04258ff636a847ff79548f6906ce3e5f1df19b95
Hao Zhao <36955586+eherozhao@users.noreply.github.com>
2022-04-29 16:40:39 -0700

    add browserslist eslint check for compatibility (#120)
    * add browserslist eslint check for compatibility
    
    * add webpack target to browserlist for determinate es-features that may be used to generate a runtime-code && refactor the js files in the root directory by prettier

--- karma.conf.js
+++ karma.conf.js
@@ -14,7 +14,7 @@ const TEST_BROWSERS =
     : ['Chrome'];
 console.log(`TEST_BROWSERS=${TEST_BROWSERS.join(',')}`);
 
-module.exports =function(config) {
+module.exports = function(config) {
   config.set({
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
